5767f2af1ce2202caceef17853c0a1f813aef625
Vadim <121517593+Arhizmei@users.noreply.github.com>
2023-02-04 21:41:38 +0200

    Initial commit

--- app/src/main/java/com/zmei/shop/MainActivity.java
+++ app/src/main/java/com/zmei/shop/MainActivity.java
@@ -3,8 +3,11 @@ package com.zmei.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+<<<<<<< HEAD
 import android.view.View;
 import android.widget.TextView;
+=======
+>>>>>>> 8adb26b (Initial commit)
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+<<<<<<< HEAD
 
     public void increaseQuantity(View view) {
         TextView quantitY = findViewById(R.id.quantitY);
         quantitY.setText("1");
     }
+=======
+>>>>>>> 8adb26b (Initial commit)
 }
